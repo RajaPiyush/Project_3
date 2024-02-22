ddb175c0638a11a854814bf2f22859723c8c7fd0
Raja Piyush <90242296+RajaPiyush@users.noreply.github.com>
2024-02-22 19:36:23 +0530

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Joker2WepApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
