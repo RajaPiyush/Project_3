7aa2694b75dc6877251b8d618d62a748cc3ec044
Raja Piyush <rajapiyush24032001@gmail.com>
2024-02-23 12:39:43 +0530

    Added bin and data files

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Joker2WepApp.Models;
+
+namespace Joker2WepApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Joker2WepApp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
